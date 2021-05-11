6f8f7f491ed1776bfb24a156fe8baebe04976a49
Diyar Kudrat <dkudrat@gmail.com>
2021-05-11 11:08:24 -0700

    added post mutation which allows you to post new links

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 const { ApolloServer } = require('apollo-server');
-
-const typeDefs = `
-  type Query {
-      info: String!
-      feed: [Link!]!
-  }
-
-  type Link {
-      id: ID!
-      description: String!
-      url: String!
-  }
-`
+const fs = require('fs');
+const path = require('path');
 
 let links = [{
     id: 'link-0',
@@ -19,20 +8,31 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
 }];
 
+let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link);
+
+            return link;
+        }
+    },
 }
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers
 })
 
