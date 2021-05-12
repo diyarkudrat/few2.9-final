7f011393cb376ee52166c8c714d6bb8f6a74d56d
Diyar Kudrat <dkudrat@gmail.com>
2021-05-12 11:02:32 -0700

    implemented GraphQL subscriptions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
+const { PubSub } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 const { getUserId } = require('./utils');
@@ -7,16 +8,19 @@ const Query = require('./resovlers/Query');
 const Mutation = require('./resolvers/Mutation');
 const Link = require('./resolvers/Link');
 const User = require('./resolvers/User');
+const Subscription = require('./resolvers/Subscription');
 
 
 const resolvers = {
     Query,
     Mutation,
     User,
-    Link
+    Link,
+    Subscription
 }
 
 const prisma = new PrismaClient();
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
@@ -28,6 +32,7 @@ const server = new ApolloServer({
         return {
             ...req,
             prisma,
+            pubsub,
             userId:
               req && req.headers.authorization
                 ? getUserId(req)
